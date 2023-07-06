591d0fdff58a552ae0f2c64a436a4cebbf57285b
Iago_saito <iagoosaito@gmail.com>
2023-07-06 19:33:06 -0400

    feat: rota teste adicionada

--- app/Teste/teste.py
+++ app/Teste/teste.py
@@ -4,21 +4,48 @@ import pandas as pd
 from fastapi import APIRouter
 from imblearn.over_sampling import BorderlineSMOTE
 from sklearn.model_selection import train_test_split
-import requests
-from io import StringIO
 
-url = "https://drive.google.com/file/d/1ZSYC8leY7MyMqS3dCehkkZLZej_7ON4E/view?usp=sharing"
-
-file_id = url.split('/')[-2]
-dwn_url = 'https://drive.google.com/uc?export=download&id=' + file_id
-url2 = requests.get(dwn_url).text
-csv_raw = StringIO(url2)
-df = pd.read_csv(csv_raw)
-print(df.head())
-
+url = "https://media.githubusercontent.com/media/Iagoakiosaito/dataset-pd/main/creditcard.csv"
 router = APIRouter()
+df = pd.read_csv(url, on_bad_lines='skip')
 
 
-@router.get("/")
-def read_root():
-    return {"Hello": "World"}
+X = df.drop(['Class'], axis=1)
+Y = df["Class"]
+# getting just the values for the sake of processing
+# (it's a numpy array with no columns)
+xData = X.values
+yData = Y.values
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)  # 80% Treino e 20% Teste
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2,
+                                                  random_state=42)  # do Treino 20% é Validação
+
+attributes = pd.DataFrame(X_train)
+labels = pd.Series(y_train, name='class')
+df_train = pd.concat([attributes, labels], axis=1)
+
+smote = BorderlineSMOTE(k_neighbors=3, m_neighbors=2, sampling_strategy='minority')
+X_smote, y_smote = smote.fit_resample(X, Y)
+
+df_train_smt = pd.DataFrame(X_smote, columns=df_train.columns)
+df_train_smt['class'] = y_smote
+
+
+@router.get("/api")
+async def readCSV():
+    try:
+
+        # Frequencia das classes com gráfico de barras
+        freq_class = sorted(Counter(df_train['class']).items())
+        return {"Normal": freq_class[0][1], "Fraude": freq_class[1][1]}
+
+    except Exception as e:
+        return {"error": str(e)}
+
+
+@router.get("/scatterData")
+async def scatterData():
+    x = df_train_smt['V14']
+    y = df_train_smt['V4']
+    return {"V14": x, "V4": y}
